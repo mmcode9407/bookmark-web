940c522d5258f5bf3ad0c5e5039eddbec3c49335
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-05-20 20:51:51 +0200

    feat: create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Footer } from "./components/Footer/Footer";
 import { Navigation } from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
       <Navigation />
+
+      <main style={{ height: "100vh" }}></main>
+
+      <Footer />
     </>
   );
 }
